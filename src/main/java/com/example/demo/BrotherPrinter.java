1e576c73a90f01e5501dcc7855a1a8be412893e1
Hank <4A790162@stust.edu.tw>
2024-05-16 00:24:02 +0800

    Rename APrinter to BrotherPrinter, add HpPrinter, and practice @Qualifier

--- src/main/java/com/example/demo/BrotherPrinter.java
+++ src/main/java/com/example/demo/BrotherPrinter.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class APrinter implements Printer {
+public class BrotherPrinter implements Printer {
 
     @Override
     public void print(String message) {
-        System.out.println("A印表機："+message);
+        System.out.println("Brother印表機："+message);
     }
 }
