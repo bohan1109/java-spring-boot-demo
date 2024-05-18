c4c78f4f42d82234e990735bbb1d378ace0c5a0d
Hank <4A790162@stust.edu.tw>
2024-05-18 23:43:18 +0800

    Revert HpPrinter to basic version and remove application.properties configuration

--- src/main/java/com/example/demo/HpPrinter.java
+++ src/main/java/com/example/demo/HpPrinter.java
@@ -7,17 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class HpPrinter implements Printer {
 
-    @Value("${printer.count:100}")
-    private int count;
-
-    @Value("${printer.name:Bryan}")
-    private String name;
-
     @Override
     public void print(String message){
-        count--;
-        System.out.println(name+ "："+message);
-        System.out.println("剩餘使用次數"+count);
+        System.out.println("Hp印表機：");
     }
 
 
