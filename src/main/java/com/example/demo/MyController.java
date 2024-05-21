d8ec4ecb4e795c1350e955ceb254482116e9c294
Hank <4A790162@stust.edu.tw>
2024-05-21 20:49:20 +0800

    Revert to basic version by removing extra files

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,20 +1,9 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-    @Autowired
-    private Printer printer;
-
-    @RequestMapping("/test")
-    public String test(){
-        printer.print("test");
-        return "hello world";
-
-    }
 }
