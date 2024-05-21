c3858335bcdcd0a1fdae7b085683c653abb88518
Hank <4A790162@stust.edu.tw>
2024-05-22 00:11:28 +0800

    Add multiple @RequestParam parameters to MyController for practice

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+    @RequestMapping("/test1")
+    public String test1 (@RequestParam Integer id,
+                         @RequestParam(name = "name",required = false,defaultValue = "10") String name){
+        System.out.println("id:"+id);
+        System.out.println("name:"+name);
+        return "test1";
+    }
 
 }
