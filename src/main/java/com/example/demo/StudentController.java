767554bad37722e9e7c721d555aa5e5a60fcd274
Hank <4A790162@stust.edu.tw>
2024-05-30 23:02:54 +0800

    Modify ID to auto-generate and add batch insert for Student entity

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,12 +20,27 @@ public class StudentController {
 
     @PostMapping("/students")
     public String insert(@RequestBody Student student){
-        String sql = "INSERT INTO student(id,name) VALUES (:studentId,:studentName)";
+        String sql = "INSERT INTO student(name) VALUES (:studentName)";
         Map<String, Object> map = new HashMap<>();
-        map.put("studentId", student.getId());
         map.put("studentName", student.getName());
-        namedParameterJdbcTemplate.update(sql,map);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        namedParameterJdbcTemplate.update(sql,new MapSqlParameterSource(map),keyHolder);
+        int id = keyHolder.getKey().intValue();
+        System.out.println("自動生成的id為："+id);
         return "insert success";
+    }
+
+    @PostMapping("/students/batch")
+    public String insertList(@RequestBody List<Student> studentList){
+        String sql = "INSERT INTO student(name) VALUES (:name)";
+        MapSqlParameterSource[] parameterSource = new MapSqlParameterSource[studentList.size()];
+        for (int i = 0; i < studentList.size(); i++) {
+            Student student = studentList.get(i);
+            parameterSource[i] = new MapSqlParameterSource();
+            parameterSource[i].addValue("name", student.getName());
+        }
+        namedParameterJdbcTemplate.batchUpdate(sql,parameterSource);
+        return "insert studentList success";
     }
 
     @PutMapping("/students/{studentId}")
