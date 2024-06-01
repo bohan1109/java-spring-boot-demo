4fec335f0246a72f3b0a4e390b8d3786fcf4f53c
Hank <4A790162@stust.edu.tw>
2024-06-01 22:40:12 +0800

    Add StudentController, Student entity, and StudentRepository using Spring Data JPA

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,2 +1,33 @@
-package com.example.demo;public class Student {
+package com.example.demo;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    Integer id;
+
+    @Column(name = "name")
+    String name;
 }
