c3cf896d66cfb84d50be5e6e8698a7ec421957ce
Hank <4A790162@stust.edu.tw>
2024-06-02 00:14:18 +0800

    Add CRUD operations in StudentController

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,9 +1,9 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -14,6 +14,32 @@ public class StudentController {
     @PostMapping("/students")
     public String insert(@RequestBody Student student){
         studentRepository.save(student);
-        return "insert success";
+        return "執行insert操作";
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getOneStudents(@PathVariable int studentId){
+        Student student = studentRepository.findById(studentId).orElse(null);
+        return student;
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String updateStudent(@PathVariable int studentId, @RequestBody Student student){
+        Student existingStudent  = studentRepository.findById(studentId).orElse(null);
+        if(existingStudent  != null){
+            existingStudent .setId(studentId);
+            studentRepository.save(student);
+            return "執行Update操作";
+        }
+        else {
+            return "找不到id為"+studentId+"的資料";
+        }
+
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String deleteStudent(@PathVariable int studentId){
+        studentRepository.deleteById(studentId);
+        return "執行Delete操作";
     }
 }
